3317feb78747d66463af89782dab023d73bad50b
Alex Naser <c.a.naser@gmail.com>
2013-06-21 09:15:15 -0400

    refactor into dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Photogur::Application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
-  get 'pictures/0' => 'pictures#picture0'
-  get 'pictures/1' => 'pictures#picture1'
-  get 'pictures/2' => 'pictures#picture2'
+  get 'pictures/:id' => 'pictures#show', as: "picture"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
